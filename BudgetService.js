84049ef564141272a37046ddd36b87f6d8bb16ac
Johnny <>
2022-10-08 17:11:22 +0800

    add head & tail

--- BudgetService.js
+++ BudgetService.js
@@ -4,26 +4,48 @@ class BudgetService {
   query(start, end) {
     if (dayjs(end).isAfter(start)) {
       const days = dayjs(end).diff(start, 'days') + 1;
-      const budgets = this.getBudgets();
       if (dayjs(end).diff(start, 'month') == 0) {
         // 非跨月
-
-        const selectedBudget = budgets.filter((b) => {
-          return b.getYearMonth().isSame(start, 'month');
-        });
-        console.log(selectedBudget);
-        if (selectedBudget.length > 0) {
-          console.log(selectedBudget);
-          console.log(days);
-          const daysInMonth = selectedBudget[0].getYearMonth().daysInMonth();
-          return selectedBudget[0].getAmount() / daysInMonth * days;
-        }
+        const selectedBudget = this.getBudgetInMonth(start.format('M'));
+        const daysInMonth = selectedBudget.getYearMonth().daysInMonth();
+        return selectedBudget.getAmount() / daysInMonth * days;
+      } else {
+        const startMonthOfBuget = this.getStartMonthOfBuget(start);
+        const endMonthOfBuget = this.getEndMonthOfBuget(end);
+        const middleMonthOfBuget = this.getMiddleMonthOfBuget(start, end);
+        return startMonthOfBuget + middleMonthOfBuget + endMonthOfBuget;
       }
-      return 50;
     } else {
       // Invaild data
       return 0;
     }
+  }
+
+  getBudgetInMonth(month) {
+    const budgets = this.getBudgets();
+    const selectedBudget = budgets.find((b) => {
+      return b.getYearMonth().format('M') === month;
+    });
+    return selectedBudget;
+  }
+
+  getStartMonthOfBuget(start) {
+    const daysInMonth = start.daysInMonth();
+    const lastDayInMonth = start.endOf('month');
+    const days = lastDayInMonth.diff(start, 'days') + 1;
+    const selectedBudget = this.getBudgetInMonth(start.format('M'));
+    return selectedBudget.getAmount() / daysInMonth * days;
+  }
+
+  getMiddleMonthOfBuget(start, end) {
+    return 0;
+  }
+
+  getEndMonthOfBuget(end) {
+    const daysInMonth = end.daysInMonth();
+    const days = end.format('D');
+    const selectedBudget = this.getBudgetInMonth(end.format('M'));
+    return selectedBudget.getAmount() / daysInMonth * days;
   }
 
   getBudgets() {
--- BudgetService.test.js
+++ BudgetService.test.js
@@ -26,7 +26,52 @@ describe('qrery', () => {
     budgetService.getBudgets = jest.fn().mockReturnValue([
       new Budget('201010', 310)
     ]);
-    const result = budgetService.query(new Date('2010-10-01'), new Date('2010-10-31'));
+    const result = budgetService.query(dayjs('2010-10-01'), dayjs('2010-10-31'));
     expect(result).toBe(310);
+  });
+
+
+  it('query two month', () => {
+    const budgetService = new BudgetService();
+    budgetService.getBudgets = jest.fn().mockReturnValue([
+      new Budget('201010', 310),
+      new Budget('201011', 30),
+    ]);
+    const result = budgetService.query(dayjs('2010-10-01'), dayjs('2010-11-30'));
+    expect(result).toBe(310 + 30);
+  });
+
+
+
+  it('query two month check head', () => {
+    const budgetService = new BudgetService();
+    budgetService.getBudgets = jest.fn().mockReturnValue([
+      new Budget('201010', 310),
+      new Budget('201011', 30),
+    ]);
+    const result = budgetService.query(dayjs('2010-10-30'), dayjs('2010-11-30'));
+    expect(result).toBe(20 + 30);
+  });
+
+  it('query two month check tail', () => {
+    const budgetService = new BudgetService();
+    budgetService.getBudgets = jest.fn().mockReturnValue([
+      new Budget('201010', 310),
+      new Budget('201011', 30),
+    ]);
+    const result = budgetService.query(dayjs('2010-10-01'), dayjs('2010-11-02'));
+    expect(result).toBe(310 + 2);
+  });
+
+
+  it('query three month', () => {
+    const budgetService = new BudgetService();
+    budgetService.getBudgets = jest.fn().mockReturnValue([
+      new Budget('201010', 310),
+      new Budget('201011', 30),
+      new Budget('201012', 3100)
+    ]);
+    const result = budgetService.query(dayjs('2010-10-01'), dayjs('2010-12-31'));
+    expect(result).toBe(310 + 30 + 3100);
   });
 });
